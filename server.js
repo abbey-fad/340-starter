1828e69d02efce376d5239fa2a37c817c3baf50c
abbey-fad <adeabbey212@gmail.com>
2025-03-18 21:09:48 +0100

    error

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index") // Added utilities to scope
 
 /* ***********************
